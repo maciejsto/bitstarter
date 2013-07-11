5844962316a555be77bc9b99e35bcb89405e4f16
Maciej Stoklosa <maciejsto@gmail.com>
2013-07-11 20:26:37 +0000

    commit

--- web.js
+++ web.js
@@ -1,12 +1,35 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var readFile = function(file) {
+    var d;
+    fs.readFileSync(file, function(err, data){
+	if (err) throw err;
+	var d = data;
+    });
+    console.log(d);
+    return d;
+    
+};
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var buflen = readFile.lenght;  
+  var buffer = new Buffer(buflen);
+  var str = buffer.toString('utf-8');
+  //TODO 
+  console.log(str);
+
+
+ 
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
