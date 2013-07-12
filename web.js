1d263943b422e592e7725f31f34f17a61b7f6034
Maciej Stoklosa <maciejsto@gmail.com>
2013-07-12 07:29:28 +0000

    upd

--- web.js
+++ web.js
@@ -31,7 +31,7 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-    console.log(readFile(filename);
+    console.log(readFile(filename));
 
 });
 
