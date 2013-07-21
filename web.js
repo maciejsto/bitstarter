bf3909abc26d0f5d8dc3f1e299622678e2ad418a
Maciej Stoklosa <maciejsto@gmail.com>
2013-07-21 21:23:32 +0000

    adding files

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 //var filename = '/home/ubuntu/myrepo/bitstarter/index.html';
 
 var app = express.createServer(express.logger());
@@ -29,7 +30,7 @@ var buf = fs.readFileSync('./index.html','utf8');
   //response.send('sssss');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
    // console.log(readFile(filename));
